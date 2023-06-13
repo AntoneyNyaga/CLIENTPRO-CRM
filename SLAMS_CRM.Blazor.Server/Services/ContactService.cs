04c328e49c8b8bccd5f4489dd46795c01204242b
Fredrick Ochieng <93923470+MaureenWagema@users.noreply.github.com>
2023-06-13 10:08:14 +0300

    minor fix on email template. More changes to come.

---
+++ SLAMS_CRM.Blazor.Server/Services/ContactService.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Xpo;
+using SLAMS_CRM.Module.BusinessObjects.CustomerManagement;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SLAMS_CRM.Blazor.Server.Services
+{
+    public interface IContactService
+    {
+        Task<List<Contact>> GetContacts();
+    }
+
+    public class ContactService : IContactService
+    {
+        private readonly IObjectSpaceProvider objectSpaceProvider;
+
+        public ContactService(IObjectSpaceProvider objectSpaceProvider)
+        {
+            this.objectSpaceProvider = objectSpaceProvider;
+        }
+
+        public async Task<List<Contact>> GetContacts()
+        {
+            return await Task.Run(() =>
+            {
+                using (var objectSpace = objectSpaceProvider.CreateObjectSpace())
+                {
+                    var xpObjectSpace = (XPObjectSpace)objectSpace;
+                    var unitOfWork = xpObjectSpace.Session as UnitOfWork;
+
+                    var contacts = new XPQuery<Contact>(unitOfWork).OrderBy(c => c.Account.Name).ToList();
+                    // Perform additional operations or filtering as needed
+
+                    return contacts;
+                }
+            });
+        }
+
+    }
+}
