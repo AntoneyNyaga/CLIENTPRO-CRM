875b7e8e4d84e78a1e4f434c446ebe233e859638
Fredrick Ochieng <93923470+MaureenWagema@users.noreply.github.com>
2023-06-29 14:20:14 +0300

    Added notifications to view user assigned jobs

---
+++ CLIENTPRO_CRM.Module/BusinessObjects/Notification.cs
@@ -0,0 +1,78 @@
+﻿using CLIENTPRO_CRM.Module.BusinessObjects.Basics;
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace CLIENTPRO_CRM.Module.BusinessObjects
+{
+    [ImageName("BO_Notifications")]
+    public class Notification : BaseObject
+    {
+        public Notification(Session session) : base(session)
+        {
+        }
+
+        public override void AfterConstruction() { base.AfterConstruction(); }
+
+        public string Message { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public class NotificationService
+        {
+            private Session session;
+
+            public NotificationService(Session session) { this.session = session; }
+
+            // This method generates notifications based on some criteria--Tasks for this case
+            public List<Notification> GetNotificationsForCurrentUser()
+            {
+                List<Notification> notifications = new List<Notification>();
+                List<BasicTask> tasks = GetAssignedTasksForCurrentUser();
+                DateTime notificationThreshold = DateTime.Now.AddDays(2); // Define the threshold for approaching deadlines
+
+                foreach(BasicTask task in tasks)
+                {
+                    if(task.DueDate <= notificationThreshold)
+                    {
+                        // Create a notification object
+                        Notification notification = new Notification(session)
+                        {
+                            Message = $"Task '{task.Subject}' is due on {task.DueDate.ToShortDateString()}.",
+                            Timestamp = DateTime.Now
+                        };
+
+                        notifications.Add(notification);
+                    }
+                }
+
+                return notifications;
+            }
+
+            private List<BasicTask> GetAssignedTasksForCurrentUser()
+            {
+                // Retrieve the current user's ID or any other identifier
+                ApplicationUser currentUser = session.GetObjectByKey<ApplicationUser>(SecuritySystem.CurrentUserId);
+
+                // Check if the current user is valid
+                if(currentUser != null)
+                {
+                    // Implement the logic to query the DB and retrieve assigned tasks for the current user
+                    List<BasicTask> assignedTasks = session.Query<BasicTask>()
+                        .Where(task => task.AssignedTo == currentUser)
+                        .ToList();
+
+                    return assignedTasks;
+                }
+
+                // Return an empty list if the current user is not found or invalid
+                return new List<BasicTask>();
+            }
+        }
+    }
+}
---
+++ CLIENTPRO_CRM.Module/Controllers/NotificationViewController.cs
@@ -0,0 +1,64 @@
+﻿using CLIENTPRO_CRM.Module.BusinessObjects;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CLIENTPRO_CRM.Module.Controllers
+{
+    public partial class NotificationViewController : ViewController<DashboardView>
+    {
+        public NotificationViewController()
+        {
+            // Register the notification logic to be executed when the View is activated
+            TargetViewNesting = Nesting.Root;
+            TargetViewType = ViewType.DashboardView;
+
+            // Create a simple action for viewing notifications
+            var viewNotificationsAction = new SimpleAction(this, "ViewNotifications", PredefinedCategory.View)
+            {
+                Caption = "Notifications",
+                ImageName = "BO_Notifications",
+                //PaintStyle = ActionItemPaintStyle.CaptionAndImage,
+                PaintStyle = ActionItemPaintStyle.Image
+            };
+            viewNotificationsAction.Execute += ViewNotificationsAction_Execute;
+        }
+
+        private void ViewNotificationsAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            var objectSpace = View.ObjectSpace;
+            var session = ((XPObjectSpace)objectSpace).Session;
+
+            // Retrieve notifications for the current user
+            var notificationService = new Notification.NotificationService(session);
+            List<Notification> notifications = notificationService.GetNotificationsForCurrentUser();
+
+            if (notifications.Count > 0)
+            {
+                // Build the notification message
+                var message = string.Join(Environment.NewLine, notifications.Select(n => n.Message));
+
+                // Show the notification in a message box
+                Application.ShowViewStrategy.ShowMessage(message, InformationType.Info);
+            }
+            else
+            {
+                // No notifications to display
+                Application.ShowViewStrategy.ShowMessage("No new notifications.", InformationType.Info);
+            }
+        }
+
+    }
+}
